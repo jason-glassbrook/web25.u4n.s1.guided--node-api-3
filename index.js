47e99dfd0c6bfee81485e3f9fff7281fab93ce1f
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-15 12:49:44 -0500

    write and use custom echo

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const routers = {
 
 server.use (express.json ())
 server.use (logger)
+server.use (echo)
+
 server.use ('/api/hubs', routers.hubs)
 
 server.listen (4000, () => {
@@ -21,4 +23,10 @@ function logger (ri, ro, next) {
   const { method, originalUrl } = ri
   console.log (`request: ${method} ${originalUrl}`)
   next ()
+}
+
+function echo (ri, ro, next) {
+  const { body } = ri
+  console.log (`request body:`)
+  console.log (body)
 }
