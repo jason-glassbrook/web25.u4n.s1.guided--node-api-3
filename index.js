6e699f74348d286c3d964665671c55d16e149a47
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-15 12:48:32 -0500

    connect hubs router

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const server = require('./server.js');
+const server = require ('./server.js')
+
+const routers = {
+  hubs : require ('./hubs/hubs-router'),
+  // messages : require ('./messages/messages-router'),
+}
+
+server.use ('/api/hubs', routers.hubs)
+
+server.listen (4000, () => {
+  console.log ('\n* Server Running on http://localhost:4000 *\n')
+})
+
 
 server.listen(4000, () => {
   console.log('\n* Server Running on http://localhost:4000 *\n');
